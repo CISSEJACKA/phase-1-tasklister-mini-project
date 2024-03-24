0e9888dc65497392c748a5bf754b4a33a569c084
JACKA <jackarcisse@gmail.com>
2024-03-24 20:13:49 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description");
+    const taskDescription = taskInput.value;
+    const taskElement = document.createElement("li");
+    taskElement.textContent = taskDescription;
+    const taskList = document.getElementById("tasks");
+    taskList.appendChild(taskElement);
+    taskInput.value = "";
+  });
 });
+
